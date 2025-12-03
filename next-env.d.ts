78034e3a957e114facd30adfc1b5d083577ea7bc
TinyBila <bilatinym@gmail.com>
2025-12-03 17:43:17 +0200

    config update

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should be committed. It is required by Next.js TypeScript projects.
+export {};
